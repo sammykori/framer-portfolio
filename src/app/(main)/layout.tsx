bbe69fa8c2c6f108016df255980e4a2065a5e664
Sammy Kori <sammykori72@gmail.com>
2026-01-19 11:06:07 +0000

    horizontal scroll content

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
 }) {
     return (
         <div className="flex min-h-screen h-[1000vh] bg-white">
-            <div className="flex fixed top-0 left-0 flex-col w-full mx-auto min-h-screen h-screen">
+            <div className="flex sticky top-0 left-0 overflow-hidden flex-col w-full mx-auto min-h-screen h-screen">
                 <NavigationMenu />
                 {children}
             </div>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -26,119 +26,234 @@ export default function Home() {
     const text4p1Rotate = useTransform(scrollYProgress, [0.11, 0.16], [0, 90])
     const text4p2Rotate = useTransform(scrollYProgress, [0.11, 0.16], [90, 0])
 
+    const text1d1Opacity = useTransform(scrollYProgress, [0.3, 0.34], [1, 0])
+    const text1d2Opacity = useTransform(scrollYProgress, [0.3, 0.34], [0, 1])
+    const text1d1Rotate = useTransform(scrollYProgress, [0.3, 0.34], [0, 90])
+    const text1d2Rotate = useTransform(scrollYProgress, [0.3, 0.34], [90, 0])
+
+    const text2d1Opacity = useTransform(scrollYProgress, [0.33, 0.38], [1, 0])
+    const text2d2Opacity = useTransform(scrollYProgress, [0.33, 0.38], [0, 1])
+    const text2d1Rotate = useTransform(scrollYProgress, [0.33, 0.38], [0, 90])
+    const text2d2Rotate = useTransform(scrollYProgress, [0.33, 0.38], [90, 0])
+
+    const text3d1Opacity = useTransform(scrollYProgress, [0.37, 0.42], [1, 0])
+    const text3d2Opacity = useTransform(scrollYProgress, [0.37, 0.42], [0, 1])
+    const text3d1Rotate = useTransform(scrollYProgress, [0.37, 0.42], [0, 90])
+    const text3d2Rotate = useTransform(scrollYProgress, [0.37, 0.42], [90, 0])
+
+    const text4d1Opacity = useTransform(scrollYProgress, [0.41, 0.46], [1, 0])
+    const text4d2Opacity = useTransform(scrollYProgress, [0.41, 0.46], [0, 1])
+    const text4d1Rotate = useTransform(scrollYProgress, [0.41, 0.46], [0, 90])
+    const text4d2Rotate = useTransform(scrollYProgress, [0.41, 0.46], [90, 0])
+
     const text5p1Opacity = useTransform(scrollYProgress, [0.12, 0.17], [1, 0])
     const text5p1Rotate = useTransform(scrollYProgress, [0.12, 0.17], [0, 90])
 
-    return (
-        <div className="flex-1 grow items-center justify-center bg-white px-20 pb-20 font-nixie">
-            <main className="h-full flex w-full bg-white ">
-                <div className="w-full flex flex-col justify-between items-start">
-                    <motion.ul
-                        variants={staggerParent}
-                        initial="hidden"
-                        animate="visible"
-                        className="text-9xl leading-40"
-                    >
-                        <motion.li
-                            variants={staggerChildX}
-                            className="h-40 relative"
-                        >
-                            <FlippingText
-                                textOpacity={text1p1Opacity}
-                                textRotate={text1p1Rotate}
-                            >
-                                SIMPLICITY
-                            </FlippingText>
-                            <FlippingText
-                                textOpacity={text1p2Opacity}
-                                textRotate={text1p2Rotate}
-                            >
-                                ALLOW ME TO
-                            </FlippingText>
-                        </motion.li>
+    const imageOpacity = useTransform(scrollYProgress, [0.05, 0.16], [0, 1])
+    const imageRotate = useTransform(scrollYProgress, [0.12, 0.17], [0, 90])
 
-                        <motion.li
-                            variants={staggerChildX}
-                            className="h-40 relative"
+    const contentX = useTransform(
+        scrollYProgress,
+        [0, 0.18, 0.3, 1],
+        ["0%", "0%", "-50%", "-50%"]
+    )
+
+    return (
+        <div className="flex-1 grow items-center justify-center bg-white font-nixie">
+            <main className="h-full flex w-full bg-white ">
+                <motion.section
+                    style={{ x: contentX }}
+                    className="h-full w-[200vw] flex px-20"
+                >
+                    <div className="w-[60vw] flex flex-col justify-between items-start">
+                        <motion.ul
+                            variants={staggerParent}
+                            initial="hidden"
+                            animate="visible"
+                            className="text-8xl 2xl:text-9xl leading-30 2xl:leading-40"
                         >
-                            <FlippingText
-                                textOpacity={text2p1Opacity}
-                                textRotate={text2p1Rotate}
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative"
                             >
-                                IS THE
-                            </FlippingText>
-                            <FlippingText
-                                textOpacity={text2p2Opacity}
-                                textRotate={text2p2Rotate}
+                                <FlippingText
+                                    textOpacity={text1p1Opacity}
+                                    textRotate={text1p1Rotate}
+                                >
+                                    SIMPLICITY
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text1p2Opacity}
+                                    textRotate={text1p2Rotate}
+                                >
+                                    ALLOW ME TO
+                                </FlippingText>
+                            </motion.li>
+
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative"
                             >
-                                INTRODUCE
-                            </FlippingText>
-                        </motion.li>
-                        <motion.li
-                            variants={staggerChildX}
-                            className="h-40 relative"
+                                <FlippingText
+                                    textOpacity={text2p1Opacity}
+                                    textRotate={text2p1Rotate}
+                                >
+                                    IS THE
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text2p2Opacity}
+                                    textRotate={text2p2Rotate}
+                                >
+                                    INTRODUCE
+                                </FlippingText>
+                            </motion.li>
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative"
+                            >
+                                <FlippingText
+                                    textOpacity={text3p1Opacity}
+                                    textRotate={text3p1Rotate}
+                                >
+                                    ULTIMATE
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text3p2Opacity}
+                                    textRotate={text3p2Rotate}
+                                >
+                                    MYSELF
+                                </FlippingText>
+                            </motion.li>
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative"
+                            >
+                                <FlippingText
+                                    textOpacity={text4p1Opacity}
+                                    textRotate={text4p1Rotate}
+                                >
+                                    SOPHISTICATION
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text4p2Opacity}
+                                    textRotate={text4p2Rotate}
+                                >
+                                    I&apos;M SAMMY KORI
+                                </FlippingText>
+                            </motion.li>
+                        </motion.ul>
+                        <motion.div
+                            variants={slideUp}
+                            initial="hidden"
+                            animate="visible"
+                            className="self-end absolute bottom-10 right-10"
                         >
-                            <FlippingText
-                                textOpacity={text3p1Opacity}
-                                textRotate={text3p1Rotate}
+                            <motion.h1
+                                style={{
+                                    opacity: text5p1Opacity,
+                                    rotateX: text5p1Rotate,
+                                    transformOrigin: "top",
+                                }}
+                                className="text-4xl"
                             >
-                                ULTIMATE
-                            </FlippingText>
-                            <FlippingText
-                                textOpacity={text3p2Opacity}
-                                textRotate={text3p2Rotate}
-                            >
-                                MYSELF
-                            </FlippingText>
-                        </motion.li>
-                        <motion.li
-                            variants={staggerChildX}
-                            className="h-40 relative"
-                        >
-                            <FlippingText
-                                textOpacity={text4p1Opacity}
-                                textRotate={text4p1Rotate}
-                            >
-                                SOPHISTICATION
-                            </FlippingText>
-                            <FlippingText
-                                textOpacity={text4p2Opacity}
-                                textRotate={text4p2Rotate}
-                            >
-                                I&apos;M SAMMY KORI
-                            </FlippingText>
-                        </motion.li>
-                    </motion.ul>
+                                - LEONARDO DA VINCI
+                            </motion.h1>
+                        </motion.div>
+                    </div>
                     <motion.div
-                        variants={slideUp}
-                        initial="hidden"
-                        animate="visible"
-                        className="self-end "
+                        style={{ opacity: imageOpacity }}
+                        className="relative h-full w-[80vw]"
                     >
-                        <motion.h1
+                        <Image
+                            src="/img/main_img.png"
+                            alt="Hero Image"
+                            fill
                             style={{
-                                opacity: text5p1Opacity,
-                                rotateX: text5p1Rotate,
-                                transformOrigin: "top",
+                                objectFit: "contain",
+                                objectPosition: "bottom",
                             }}
-                            className="text-4xl"
-                        >
-                            - LEONARDO DA VINCI
-                        </motion.h1>
+                            className="object-contain"
+                        />
                     </motion.div>
-                </div>
-                <div className="absolute h-full w-2/3 -right-[25%]">
-                    <Image
-                        src="/img/main_img.png"
-                        alt="Hero Image"
-                        fill
-                        style={{
-                            objectFit: "contain",
-                            objectPosition: "center",
-                        }}
-                        className="object-contain"
-                    />
-                </div>
+                    <div className="w-[60vw] flex flex-col justify-start items-start">
+                        <motion.ul
+                            variants={staggerParent}
+                            initial="hidden"
+                            animate="visible"
+                            className="w-full flex flex-col text-8xl 2xl:text-9xl leading-30 2xl:leading-40"
+                        >
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative flex justify-end"
+                            >
+                                <FlippingText
+                                    textOpacity={text1d1Opacity}
+                                    textRotate={text1d1Rotate}
+                                >
+                                    I AM A
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text1d2Opacity}
+                                    textRotate={text1d2Rotate}
+                                >
+                                    I LIKE TO
+                                </FlippingText>
+                            </motion.li>
+
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative flex justify-end"
+                            >
+                                <FlippingText
+                                    textOpacity={text2d1Opacity}
+                                    textRotate={text2d1Rotate}
+                                >
+                                    FRONTEND-FIRST
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text2d2Opacity}
+                                    textRotate={text2d2Rotate}
+                                >
+                                    PLAY 5-A-SIDE
+                                </FlippingText>
+                            </motion.li>
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative flex justify-end"
+                            >
+                                <FlippingText
+                                    textOpacity={text3d1Opacity}
+                                    textRotate={text3d1Rotate}
+                                >
+                                    PRODUCT
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text3d2Opacity}
+                                    textRotate={text3d2Rotate}
+                                >
+                                    ⚽️ FOOTBALL &
+                                </FlippingText>
+                            </motion.li>
+                            <motion.li
+                                variants={staggerChildX}
+                                className="h-40 relative flex justify-end"
+                            >
+                                <FlippingText
+                                    textOpacity={text4d1Opacity}
+                                    textRotate={text4d1Rotate}
+                                >
+                                    ENGINEER.
+                                </FlippingText>
+                                <FlippingText
+                                    textOpacity={text4d2Opacity}
+                                    textRotate={text4d2Rotate}
+                                >
+                                    🎱 POOL.
+                                </FlippingText>
+                            </motion.li>
+                        </motion.ul>
+                    </div>
+                </motion.section>
             </main>
         </div>
     )
