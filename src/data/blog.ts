6309ffe607583e5115730f8e935b747cb576fccd
Sammy Kori <sammykori72@gmail.com>
2026-01-22 12:05:33 +0000

    post 2 and 3

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,33 +1,35 @@
-const postTypes = ["tech", "life", "travel"] as const
+const postTypes = ['tech', 'life', 'travel', 'research'] as const
 
 const blogPosts = [
     {
         id: 1,
-        title: "Re-Connect Conference",
+        title: 'Re-Connect Conference',
         subtitle:
-            "Lessons From Re:Connect — Taking Our Podcast Beyond the Living Room",
-        date: "August 20, 2023",
-        type: "life",
-        readTime: "10",
-        imageUrl: "/img/blog/post1.jpg",
+            'Lessons From Re:Connect — Taking Our Podcast Beyond the Living Room',
+        date: 'August 20, 2023',
+        type: 'life',
+        readTime: '10',
+        imageUrl: '/img/blog/post1.jpg',
     },
     {
         id: 2,
-        title: "Exploring the Mountains",
-        subtitle: "Join us for an unforgettable experience!",
-        date: "August 20, 2023",
-        type: "travel",
-        readTime: "8",
-        imageUrl: "/img/blog/post1.jpg",
+        title: 'Reminiscing BU',
+        subtitle:
+            'What I could have done better while I was studying at Bournemouth University',
+        date: 'August 20, 2023',
+        type: 'life',
+        readTime: '8',
+        imageUrl: '/img/blog/post2.jpeg',
     },
     {
         id: 3,
-        title: "JavaScript Best Practices",
-        subtitle: "Join us for an unforgettable experience!",
-        date: "August 20, 2023",
-        type: "tech",
-        readTime: "12",
-        imageUrl: "/img/blog/post1.jpg",
+        title: 'Navigating the UK Job Market',
+        subtitle:
+            'As a Frontend Developer I try to navigate some of the challenges in the Job Market',
+        date: 'August 20, 2023',
+        type: 'tech',
+        readTime: '12',
+        imageUrl: '/img/blog/post3.jpg',
     },
 ]
 
