be6f5d88049711edabdeaad6f5a449d07dd75f4d
Sammy Kori <sammykori72@gmail.com>
2026-01-28 13:21:19 +0000

    fixed prettier

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-import BlogPostCard from '@/components/blog/BlogPostCard'
-import { postTypes, blogPosts } from '@/data/blog'
-import { useState } from 'react'
+"use client"
+import BlogPostCard from "@/components/blog/BlogPostCard"
+import { postTypes, blogPosts } from "@/data/blog"
+import { useState } from "react"
 
 export default function Home() {
-    const [active, setActive] = useState<string>('all')
+    const [active, setActive] = useState<string>("all")
     return (
         <div className="flex-1 grow items-center justify-center bg-white font-nixie pb-20">
             <main className="h-full  w-full bg-white px-4 md:px-20">
@@ -14,17 +14,17 @@ export default function Home() {
                         and Creative Inspiration
                     </h1>
                     <p className="w-full lg:w-1/2 text-lg md:text-2xl text-center mt-2 md:mt-10">
-                        {' '}
+                        {" "}
                         A personal blog/diary for my reflection and growth
                     </p>
                 </header>
                 <ul className="w-full flex flex-row justify-around items-center font-bold text-xl my-10 text-gray-400 overflow-x-auto">
                     <li
-                        onClick={() => setActive('all')}
+                        onClick={() => setActive("all")}
                         className="w-full min-w-20 flex items-center justify-center "
                     >
                         <h1
-                            className={`${active === 'all' ? 'font-bold text-black  border-b-2 border-black' : ''}`}
+                            className={`${active === "all" ? "font-bold text-black  border-b-2 border-black" : ""}`}
                         >
                             ALL
                         </h1>
@@ -36,7 +36,7 @@ export default function Home() {
                             className="w-full min-w-32 flex items-center justify-center"
                         >
                             <h1
-                                className={`${active === type ? 'font-bold text-black border-b-2 border-black' : ''}`}
+                                className={`${active === type ? "font-bold text-black border-b-2 border-black" : ""}`}
                             >
                                 {type.toUpperCase()}
                             </h1>
--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,35 +1,35 @@
-const postTypes = ['tech', 'life', 'travel', 'research'] as const
+const postTypes = ["tech", "life", "travel", "research"] as const
 
 const blogPosts = [
     {
         id: 1,
-        title: 'Re-Connect Conference',
+        title: "Re-Connect Conference",
         subtitle:
-            'Lessons From Re:Connect — Taking Our Podcast Beyond the Living Room',
-        date: 'August 20, 2023',
-        type: 'life',
-        readTime: '10',
-        imageUrl: '/img/blog/post1.jpg',
+            "Lessons From Re:Connect — Taking Our Podcast Beyond the Living Room",
+        date: "August 20, 2023",
+        type: "life",
+        readTime: "10",
+        imageUrl: "/img/blog/post1.jpg",
     },
     {
         id: 2,
-        title: 'Reminiscing BU',
+        title: "Reminiscing BU",
         subtitle:
-            'What I could have done better while I was studying at Bournemouth University',
-        date: 'August 20, 2023',
-        type: 'life',
-        readTime: '8',
-        imageUrl: '/img/blog/post2.jpeg',
+            "What I could have done better while I was studying at Bournemouth University",
+        date: "August 20, 2023",
+        type: "life",
+        readTime: "8",
+        imageUrl: "/img/blog/post2.jpeg",
     },
     {
         id: 3,
-        title: 'Navigating the UK Job Market',
+        title: "Navigating the UK Job Market",
         subtitle:
-            'As a Frontend Developer I try to navigate some of the challenges in the Job Market',
-        date: 'August 20, 2023',
-        type: 'tech',
-        readTime: '12',
-        imageUrl: '/img/blog/post3.jpg',
+            "As a Frontend Developer I try to navigate some of the challenges in the Job Market",
+        date: "August 20, 2023",
+        type: "tech",
+        readTime: "12",
+        imageUrl: "/img/blog/post3.jpg",
     },
 ]
 
